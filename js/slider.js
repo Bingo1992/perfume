29394218eafaad1a26629170864367d66f741f07
unknown <1163751581@qq.com>
2016-06-29 22:51:18 +0800

    add

--- js/slider.js
+++ js/slider.js
@@ -1,56 +1,105 @@
-	
-
 $(function() {
-	var total = $('.total-ml').html(); //总数
-	var calculate = $('.caculate-ml').html(); //统计
+	var total = $('.total-ml').html(); //总容量
+	var calculate = $('.caculate-ml').html(); //统计容量
 	var m_total = 100; //每条试管总数
-	var step = 10;
+	var step = 5;
+
     //升
-    $('body').delegate('.plus', 'touchstart', function() {
-
-        var $v_parent = $(this).parent(); //纵向
+    $('.perfume-slider').delegate('.plus', 'touchstart', function() {
+       
+       var $v_parent = $(this).parent(); //纵向
         var $tube = $v_parent.find('.min-tube');
+        var $p = $(this).siblings('.sort-p'); //判断主辅次
 
-        var $h_parent = $(this).parents('.list-box'); //横向
-        var $h_tube = $h_parent.find('.h-min-tube');
-
-
-        var $m_caculate = $v_parent.find('.m-caculate') || $h_parent.find('.m-caculate');
-        var m_caculate = parseInt($m_caculate.html());
+        var $m_caculate = $v_parent.find('.m-caculate');
+        var m_caculate = parseInt($m_caculate.html());//当前试管的值
+        // 当前试管值小于100%
         if (m_caculate < m_total) {
             m_caculate += step; //加
+            $m_caculate.html(m_caculate);
+            
+            //取消符合条件的试管对减号的禁用
+            $('.sort-p').each(function(){
+                var mc = $(this).parents('.slider').find('.m-caculate').html();
+                if($(this).hasClass('main-p') && (mc > 25) ){
+                    $(this).siblings('.minus').removeClass('btn-disable');
+                }
+                if($(this).hasClass('assist-p') && (mc > 5) ) {
+                    $(this).siblings('.minus').removeClass('btn-disable');
+                }
+            })
+            
+            // 判断是主辅次（次香一直处于禁用状态)
+            if($p.hasClass('main-p') || $p.hasClass('assist-p')){
+                if(m_caculate >= 50){
+                    // overflow_dialog('主辅香不可超过50%');
+                    $(this).addClass('btn-disable');  
+                }
+            }
         }
+
         var per = (m_caculate / m_total) * 100 + "%";
         $tube.css('height', per);
-        $h_tube.css('width', per); //横向
-        $m_caculate.html(m_caculate);
-
-        // 统计总数
-        v_caculate();
+        v_caculate();  //统计总容量
     });
 
 
     //降
-    $('body').delegate('.minus', 'touchstart', function() {
+    $('.perfume-slider').delegate('.minus', 'touchstart', function() {
         var $v_parent = $(this).parent(); //纵向
         var $tube = $v_parent.find('.min-tube');
+        var $p = $(this).siblings('.sort-p'); //判断主辅次
 
-        var $h_parent = $(this).parents('.list-box'); //横向
-        var $h_tube = $h_parent.find('.h-min-tube');
+        var $m_caculate = $v_parent.find('.m-caculate');
+        var m_caculate = parseInt($m_caculate.html());//当前试管值
 
-        var $m_caculate = $v_parent.find('.m-caculate') || $h_parent.find('.m-caculate');
-        var m_caculate = parseInt($m_caculate.html());
         if (m_caculate > 0) {
             m_caculate -= step; //减
+            $m_caculate.html(m_caculate);
+
+            //取消符合条件的试管对加号的禁用
+            $('.sort-p').each(function(){
+                var mc = parseInt($(this).parents('.slider').find('.m-caculate').html());
+                if($(this).hasClass('main-p') && (mc < 50)){
+                    $(this).siblings('.plus').removeClass('btn-disable');
+                }
+                if($(this).hasClass('assist-p') && (mc < 50)) {
+                    $(this).siblings('.plus').removeClass('btn-disable');
+                }
+            })
+
+            // 判断是主辅次
+            if($p.hasClass('main-p')){
+                if(m_caculate <= 25){
+                    // overflow_dialog('主香不可低于25%');
+                    $(this).addClass('btn-disable');  
+                }
+            }else if($p.hasClass('assist-p')){
+                if(m_caculate <= 5){
+                    // overflow_dialog('辅香不可低于5%');
+                    $(this).addClass('btn-disable');
+                }
+            }
         }
         var per = (m_caculate / m_total) * 100 + "%";
         $tube.css('height', per); //纵向
-        $h_tube.css('width', per); //横向
-        $m_caculate.html(m_caculate);
-        v_caculate();
+        
+        v_caculate();//统计总容量
 
     });
 
+    // 统计纵向试管ml总数
+    function v_caculate() {
+        var s_total = 0; //所有试管ml总数
+        $('.slider').each(function() {
+            var value = parseInt($(this).find('.m-caculate').html());
+            s_total += value;
+            if (s_total >= total) {
+                $('.plus').addClass('btn-disable');
+            }
+        });
+        $('.caculate-ml').html(s_total);
+    }
 
     //主香遮罩
     $('.btn-perfume').delegate('.btn-main', 'click', function() {
@@ -71,30 +120,15 @@ $(function() {
         perNum(2);
  	});
 
-    // 统计纵向试管ml总数
-	function v_caculate() {
-	    var s_total = 0; //所有试管ml总数
-	    $('.slider').each(function() {
-	        var value = parseInt($(this).find('.m-caculate').html());
-	        s_total += value;
-	        if (s_total >= total) {
-	            $('.plus').addClass('btn-disable');
-	        } else {
-	            $('.plus').removeClass('btn-disable');
-	        }
-	    });
-	    $('.caculate-ml').html(s_total);
-	}
-
-
-   
+ 
     //主香，辅香，次香的选择
     function perNum(i) {
         var check_num = [1, 5, 3]; //主香只可选择一种
         var per_name = ["主香", "辅香", "次香"];
+        var flag;
         $('.pro-cnt').delegate(':checkbox', 'click', function() {
-            var flag = 0;
             var _this = $(this);
+            flag = 0;
             //统计checked数目
             $(".pro-cnt :checkbox").each(function(j) {
                 var check = $(".pro-cnt :checkbox").eq(j).prop("checked");
@@ -113,18 +147,14 @@ $(function() {
                 var html2 = '您至少要选择1种' + per_name[i];
                 overflow_dialog(html2);
             }
-            if (_this.prop("checked") == true) {
-                add_h_tube(); //添加试管
-            } else {
-                remove_h_tube(flag); //减少试管
-            }
+
         });
 
         //点击确定添加纵向试管
         $('.confirm').click(function() {
-            $('.slider-container li').each(function() {
-                add_v_tube(i);
-            })
+            for(var k = 0; k < flag; k++){
+                add_v_tube(i);//多少个checkbox=checked则加几支试管
+            }
         });
     }
 
@@ -132,22 +162,23 @@ $(function() {
 
 	
 
-//显示选择遮罩
+//显示选择香基遮罩
 function pro_dialog() {
     if ($('.per-edit').html() == "完成") {
         overflow_dialog("编辑状态下不可选择");
-    } else if($('.plus').hasClass('btn-disable')){
-    	overflow_dialog("总量已达150ml，不可再添加");
+    } else if($('.caculate-ml').html == 100){
+    	overflow_dialog("总量已达100%，不可再添加");
     } else {
         $('.pro-dialog').removeClass('hide');
         $('body').addClass('fixed-body');
     }
-
+    // 返回按钮
     $('.return').click(function() {
         $('.pro-dialog').addClass('hide');
         $('body').removeClass('fixed-body');
         history.go(0); //刷新页面
     });
+    // 确定按钮
     $('.confirm').click(function() {
         $('.pro-dialog').addClass('hide');
         $('body').removeClass('fixed-body');
@@ -164,38 +195,23 @@ function overflow_dialog(html) {
     });
 }
 
-//添加横向试管
-function add_h_tube() {
-    var addHTML = '<li class="list-box">\
-                    <div class="list-info">\
-                        <span>香精名称</span>\
-                        <div class="horizational-tube"><div class="h-min-tube"></div></div></div>\
-                    <div class="amount-2">\
-                        <span class="minus icon-minus"></span>\
-                        <span class="m-caculate count">10</span>\
-                        <span class="plus icon-plus"></span>\
-                    </div></li>';
-    $('.slider-container').append(addHTML);
-}
-
-//减去横向试管
-function remove_h_tube(i) {
-    $('.slider-container').find('li').eq(i).remove(); //此处只是减去最后一个
-}
-
 //添加纵向试管
 function add_v_tube(i) {
-    var perName = ['<p class="main-p sort-p" data-num="1">主香</p>', '<p class="assist-p sort-p" data-num="2">辅香</p>', '<p class="minor-p sort-p" data-num="3">次香</p>'];
+    var perName = ['<p class="main-p sort-p" data-num="1">主香</p>', '<p class="assist-p sort-p" data-num="2">辅香</p>', '<p class="minor-p sort-p" data-num="3">次香</p>'];  
+
+    var perPlus = ['<p class="plus icon-plus"></p>','<p class="plus icon-plus"></p>','<p class="plus icon-plus btn-disable"></p>'];
+    var perNum = ['25','5','5'];//每种香基默认最低值
 
     var addHTML = '<div class="slider sort-slider">\
 						<i class="icon-cancel"></i>' +
-        perName[i] +
-        '<p class="plus icon-plus"></p>\
-		                <div class="tube"><div class="min-tube">\
+                    perName[i] + perPlus[i] +
+		                '<div class="tube"><div class="min-tube">\
 		                </div></div>\
-		                <p class="minus icon-minus"></p>\
+		                <p class="minus icon-minus btn-disable"></p>\
 		                <div class="slider-info border-top">\
-		                    <p class="gray-font"><span class="m-caculate">20</span>ml</p>\
+		                    <p class="gray-font"><span class="m-caculate">' +
+                            perNum[i] +
+                            '</span>%</p>\
 		                    <p class="gray-font border-top">玫瑰</p>\
 		                </div></div>';
 
@@ -213,17 +229,22 @@ function remove_v_tube(i) {
 //随着试管多少瓶子的变化
 function bottle_change() {
     var len = $('.slider').length;
-    if(len==0){
+    var $i = $('.add-recipe').find('i');
+    var $p1 = $i.siblings('.sort-p');
+    var $p2 = $i.siblings('.stage-p');
 
-    }else if (len < 7) {
-        $('.add-recipe').removeClass('hide')
-            .find('p').removeClass('hide')
-            .siblings('i').css('font-size', '50px');
-    }
-    if (len >= 7) {
-        $('.add-recipe').find('p').addClass('hide').siblings('i').css('font-size', '30px');
+    if( len == 0){
+        $p2.removeClass('hide');
+    }else if (len < 7 && len >= 1) {
+        $p2.addClass('hide');
+        $p1.removeClass('hide');
+        $i.css('font-size', '50px');
+
+    }else if (len >= 7) {
+        $p1.addClass('hide')
+        $i.css('font-size', '30px');
     } else if (len >= 9) {
-        $('.add-recipe').addClass('hide'); //当试管大于9个时，删除右边瓶子
+        $('.add-recipe').addClass('hide'); //当试管大于9个时，删除右边瓶子及说明
     }
 }
 
@@ -231,7 +252,7 @@ function bottle_change() {
 var asc = function(a, b) {
         return $(a).find('.sort-p').data('num') > $(b).find('.sort-p').data('num') ? 1 : -1;
     }
-    //主香、辅香、次香竖直试管排序
+//主香、辅香、次香竖直试管排序
 var sortByInput = function(sortBy) {
     var sortEle = $('.perfume-slider .sort-slider').sort(sortBy);
     $('.perfume-slider').empty().append(sortEle);
